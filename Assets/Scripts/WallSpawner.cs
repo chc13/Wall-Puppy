066b4c9bf83f1bc814f985555da5edcf52be2ac6
chc13 <90738243+chc13@users.noreply.github.com>
2023-03-09 17:32:26 -0700

    readjusted scene and prepared for wall spawning script

---
+++ Assets/Scripts/WallSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpawner : MonoBehaviour
+{
+    public GameObject walls;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void spawnWalls()
+    {
+        //Instantiate(pipe, new Vector3(transform.position.x,Random.Range(lowestPoint,highestPoint),0), transform.rotation);
+    }
+}
